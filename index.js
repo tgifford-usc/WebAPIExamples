25aaddb10a728a4aa4b2bd3fad1cd331405ef7d0
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2024-09-10 20:24:04 +1000

    Finished camera capture example

--- index.js
+++ index.js
@@ -67,20 +67,68 @@ if (window.DeviceOrientationEvent) {
 
 //-----  Code for accessing the camera -----//
 const width = 320;    // We will scale the photo width to this
-const height = 0;     // This will be computed based on the input stream
+let height = 0;     // This will be computed based on the input stream
 const streaming = false;
-let video = null;
-let canvas = null;
-let photo = null;
-let startbutton = null;
+let video = document.getElementById('video');
+let canvas = document.getElementById('canvas');
+let photo = document.getElementById('photo');
+let startbutton = document.getElementById('startbutton');
+
+
+function clearphoto() {
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+}
+
+function takepicture() {
+    const context = canvas.getContext("2d");
+    if (width && height) {
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+
+        const data = canvas.toDataURL("image/png");
+        photo.setAttribute("src", data);
+    } else {
+        clearphoto();
+    }
+}
 
 async function setupCameraExample() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
 
     let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
     video.srcObject = stream;
     video.play();
+    video.addEventListener(
+        "canplay",
+        (ev) => {
+            if (!streaming) {
+            height = (video.videoHeight / video.videoWidth) * width;
+    
+            video.setAttribute("width", width);
+            video.setAttribute("height", height);
+            canvas.setAttribute("width", width);
+            canvas.setAttribute("height", height);
+            streaming = true;
+            }
+        },
+        false,
+    );
+    
+    clearphoto();
 }
+
+  
+startbutton.addEventListener(
+    "click",
+    (ev) => {
+        takepicture();
+        ev.preventDefault();
+    },
+    false,
+);
+
